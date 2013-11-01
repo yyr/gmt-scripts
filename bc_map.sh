0c7a14000b1c8ff7ac6d078bf8521c5ceaae5621
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2013-11-02 03:07:57 +0900

    pstext

--- bc_map.sh
+++ bc_map.sh
@@ -12,7 +12,7 @@ pscoast -Rd -Dh $lats -JX7id/7id -Dh -Gchocolate $azimuth -K  > $ps
 psxyz $in ${lats}/1/160  -J -JZ2.5i -So0.1ib1 -Ggray -Wthinner \
 	-B10g10/10g10/a20:"dust":WSneZ -O -K $azimuth >> $ps
 
-# $AWK '{print $1, $2, 20, 0, 0, "RM", $3}' $in \
-#  | pstext -Rd -J -O -K -E200/40 -Gwhite -Sthinner -D-0.2i/0 >> $ps
+$AWK '{print $1, $2, 10, 0, 0, "RM", $3}' $in  \
+ | pstext $lats -J -O -K $azimuth -Sthinner  >> $ps
 
 echo "4.5 6 30 0 5 BC Dust Distribution" | pstext -R0/11/0/8.5 -Jx1i -O >> $ps
