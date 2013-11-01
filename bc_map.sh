49a2880084f14e6554b498727610a04a16f120fc
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2013-11-01 18:59:18 +0900

    better graph

--- bc_map.sh
+++ bc_map.sh
@@ -3,12 +3,16 @@
 AWK=awk
 ps=ps/map.ps
 in=data/gmt-1.txt
-pscoast -Rd -Dh -R70/90/0/35 -JX5id/5id -Dc -Gchocolate -E200/40 -K  > $ps
 
-psxyz $in -R70/90/0/35/1/100 -J -JZ2.5il -So0.3ib1 -Ggray -Wthinner \
-	-B10g10/10g10/a1p:dust:WSneZ -O -K -E200/40 >> $ps
+azimuth="-E210/20"
+lats="-R70/90/0/35"
+
+pscoast -Rd -Dh $lats -JX7id/7id -Dc -Gchocolate $azimuth -K  > $ps
+
+psxyz $in ${lats}/1/1000 -J -JZ2.5il -So0.1ib1 -Ggray -Wthinner \
+	-B10g10/10g10/a1p:dust:WSneZ -O -K $azimuth >> $ps
 
 # $AWK '{print $1, $2, 20, 0, 0, "RM", $3}' GMT-1.txt
 # 	# | pstext -Rd -J -O -K -E200/40 -Gwhite -Sthinner -D-0.2i/0 >> $ps
 
-# echo "4.5 6 30 0 5 BC AGU 2008 Membership Distribution" | pstext -R0/11/0/8.5 -Jx1i -O >> $ps
+echo "4.5 6 30 0 5 BC Dust Distribution" | pstext -R0/11/0/8.5 -Jx1i -O >> $ps
